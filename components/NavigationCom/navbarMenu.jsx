2edb551065d86e7949acd5e594e24b8b77d898aa
Jokerzzs <490619376@qq.com>
2017-06-16 13:55:25 +0800

    Update navbarMenu.jsx

--- components/NavigationCom/navbarMenu.jsx
+++ components/NavigationCom/navbarMenu.jsx
@@ -1,2 +1,27 @@
+import MenuItem from './navbarItem.jsx';
+import MenuItemTile from './navbarItemTile.jsx';
 
-sss
+export default class navbarMenuList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+        let {data,menuStyle} = this.props;
+        return (
+                <div className="acs-navbar">
+                    {data.map((el ,index)=>{
+                        switch(menuStyle){
+                            case 0:{
+                                return (<MenuItem key={index} menuData={el} hasGrandChild={el.hasGrandChild}/>)
+                            }
+                            case 1:{
+                                return (<MenuItemTile key={index} menuData={el} hasGrandChild={el.hasGrandChild}/>)
+                            }
+                        }
+                    })}
+                </div>
+            )
+    }
+
+}
